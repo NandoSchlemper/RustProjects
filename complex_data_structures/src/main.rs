363a52fe734ea62e5257d21cc688dc1f88d689b1
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-06-19 08:01:10 -0300

    hashmaps

--- complex_data_structures/src/main.rs
+++ complex_data_structures/src/main.rs
@@ -1,3 +1,12 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    // Hashmaps
+    let mut data: HashMap<_, _> = HashMap::new();
+    data.insert(String::from("Batata"), 1);
+    data.insert(String::from("Abacate"), 2);
+
+    let fruta = String::from("Abacate");
+    let search_data = data.get(&fruta).copied().unwrap_or(0);
+    println!("{}", search_data);
 }
