eb402232f03e85e396943d6307ba9ba886be24b6
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-06-18 21:56:55 -0300

    blah

--- magic_cards/src/card/card_create.rs
+++
@@ -1,9 +0,0 @@
-use crate::card::card_compose::{Tipagens, CriarCarta};
-
-
-pub fn full_card_creation() -> CriarCarta {
-    let mut user_input_nome = String::new();
-    let mut user_input_tipagem: Tipagens;
-    let mut user_input_desc: String = String::new();
-    let mut user_input_cardcolor: String = String::new();
-}
