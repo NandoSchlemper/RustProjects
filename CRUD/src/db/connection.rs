6d9b7b88d1e90851837a753e222d36f68982d545
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-07-03 00:23:42 -0300

    Modificando App-network para bridge

--- CRUD/src/db/connection.rs
+++ CRUD/src/db/connection.rs
@@ -1,12 +1,17 @@
 use tokio_postgres::{Client, Error, NoTls};
 
-pub async fn db() -> Result<Client, Error>{
-    let (client, connection) = 
-        tokio_postgres::connect("host=localhost
-                                user=grupo_do_henrique
-                                dbname=CRUD_com_rust 
-                                password=cucuz_de_frango_com_arroz_e_bom", NoTls).await?;
-    
+// =postgres://Fernando:Rfm*102030@db:5432/crud
+
+pub async fn db() -> Result<Client, Error> {
+    let (client, connection) = tokio_postgres::connect(
+        "host=db
+                user=Fernando
+                dbname=crud 
+                password=Rfm*102030",
+        NoTls,
+    )
+    .await?;
+
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("Erro na conexão com o DB >->: {}", e)
