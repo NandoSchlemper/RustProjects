1cc8dc497fb5bcb04a20f18a5c15bdba371409f2
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-07-03 07:36:11 -0300

    Mudar o Tokio-Postgres para Diesel ou sqlx, mas antes, verificar novamente compatibilidade com actix

--- CRUD/src/main.rs
+++ CRUD/src/main.rs
@@ -1,12 +1,19 @@
 mod db;
 mod web;
-use tokio_postgres::Error;
+use actix_web::{get, App, HttpResponse, HttpServer, Responder, Result};
 use db::connection::db;
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let client = db().await?;
+#[get("/")]
+async fn index() -> impl Responder {
+    "Hello! ;)"
+}
 
+#[get("/list_tables")]
+async fn list_of_tables() -> Result<impl Responder> {
+    let client = db().await.map_err(|e| {
+        HttpResponse::InternalServerError()
+            .body(format!("Falha ao conectar ao banco de dados: {}", e))
+    })?;
     let table_list_query = "
     SELECT table_name
     FROM information_schema.tables
@@ -15,10 +22,24 @@ async fn main() -> Result<(), Error> {
     ";
 
     // Passar essas queries para o arquivo src/db/queries.rs
-    let tables_list = client
-        .query(table_list_query, &[])
-        .await?;
-    println!("Tables: {:?}", tables_list);
+    let tables_list = match client.query(table_list_query, &[]).await {
+        Ok(tables) => tables,
+        Err(_) => {
+            return Ok(
+                HttpResponse::InternalServerError().body("falha na query com o banco de dados...")
+            )
+        }
+    };
 
-    Ok(())
+    Ok(HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(format!("Lista de tabelas: \n {:?}", tables_list)))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(list_of_tables))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
