c6073e9f5994d403995883eefa08c57967fb282e
Fernando Meurer Schlemper <129208500+BekaODeus@users.noreply.github.com>
2024-07-03 22:31:42 -0300

    Trocando ORM para o Diesel mesmo, configurando um Docker pra testes

--- CRUD/src/main.rs
+++ CRUD/src/main.rs
@@ -1,7 +1,6 @@
 mod db;
 mod web;
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, Result};
-use db::connection::db;
+use actix_web::{get, App, HttpServer, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -9,31 +8,8 @@ async fn index() -> impl Responder {
 }
 
 #[get("/list_tables")]
-async fn list_of_tables() -> Result<impl Responder> {
-    let client = db().await.map_err(|e| {
-        HttpResponse::InternalServerError()
-            .body(format!("Falha ao conectar ao banco de dados: {}", e))
-    })?;
-    let table_list_query = "
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-    AND table_type = 'BASE TABLE'
-    ";
-
-    // Passar essas queries para o arquivo src/db/queries.rs
-    let tables_list = match client.query(table_list_query, &[]).await {
-        Ok(tables) => tables,
-        Err(_) => {
-            return Ok(
-                HttpResponse::InternalServerError().body("falha na query com o banco de dados...")
-            )
-        }
-    };
-
-    Ok(HttpResponse::Ok()
-        .content_type("text/plain")
-        .body(format!("Lista de tabelas: \n {:?}", tables_list)))
+async fn list_of_tables() -> impl Responder {
+    "Bruh"
 }
 
 #[actix_web::main]
